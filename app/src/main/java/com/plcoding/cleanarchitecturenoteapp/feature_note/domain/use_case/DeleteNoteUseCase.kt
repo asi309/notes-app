dc488d4310100e575bccaea49ceb648d8e818c38
Asidipta <asidipta@ajnalens.com>
2025-05-23 17:42:35 +0530

    NotesViewModel In Progress

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/DeleteNoteUseCase.kt
@@ -0,0 +1,10 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class DeleteNoteUseCase(private val noteRepository: NoteRepository) {
+    suspend operator fun invoke(note: Note) {
+        noteRepository.deleteNote(note)
+    }
+}

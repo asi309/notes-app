f85716b3cc7e89817dbb93a78033b02bf9990236
Asidipta Chaudhuri <asidiptachaudhuri@gmail.com>
2025-06-03 23:57:53 +0530

    Completes Notes App Logic + presentation

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/add_edit_note/AddEditNoteEvent.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/add_edit_note/AddEditNoteEvent.kt
@@ -8,5 +8,5 @@ sealed class AddEditNoteEvent {
     data class EnteredContent(val value: String) : AddEditNoteEvent()
     data class ChangeContentFocus(val focusState: FocusState) : AddEditNoteEvent()
     data class ChangeColor(val color: Int) : AddEditNoteEvent()
-    data class SaveNote(val value: String): AddEditNoteEvent()
+    data object SaveNote: AddEditNoteEvent()
 }
